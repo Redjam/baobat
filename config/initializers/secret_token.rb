c16255c7cb340bcc7c938f052acc9b666b5e6298
Redjam <rdjamil@gmx.fr>
2014-02-14 19:00:56 +0100

    Initial commit, added Pages controller and Bootstrap-sass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaobatNew::Application.config.secret_key_base = '08ba51fc6b096734b3a14b5f0552b55c87e80c7e5ad49e7903b632c53f1a6177f730fd801d43e794fc99242ce1048c59b6c2a8e164f672d49f761e53c8d841b1'
