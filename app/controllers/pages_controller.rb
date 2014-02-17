6c7f8a96a8f7f717ed9b6b3aac1730043813019c
Redjam <rdjamil@gmx.fr>
2014-02-17 15:08:08 +0100

    Customized home page, added wh_we_are page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+	before_action :enable_home_page_header, only: [:home]	
+
   def home
+  end
+
+  def who_we_are
+  end
+
+private
+
+  def enable_home_page_header
+  	@enable_home_page_header = true
   end
 end
