26879ef6990b8caef39591e8a9c44175494e715c
Redjam <rdjamil@gmx.fr>
2014-02-28 10:28:33 +0100

    Added authentication on Statuses and Projects

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,7 @@
 class StatusesController < ApplicationController
   before_action :set_current_project
   before_action :set_status, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   
 
   # GET /statuses
